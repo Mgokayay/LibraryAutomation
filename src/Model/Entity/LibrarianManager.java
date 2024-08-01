714db55ed904ca9b2d3d55d1846a6a7573cc95b3
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-01 14:26:42 +0300

    librarianManager class created

---
+++ src/Model/Entity/LibrarianManager.java
@@ -0,0 +1,8 @@
+package src.Model.Entity;
+
+public class LibrarianManager extends Librarian{
+
+    public LibrarianManager(String name, String surname, String password, long id, String email, double salary) {
+        super(name, surname, password, id, email, salary);
+    }
+}
